297e62c3f6f5dd1d083750ba435656b850f11c4a
Riley Rice <riceriley59@gmail.com>
2023-07-28 11:04:38 -0700

    added login and register pages

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { FormEvent } from "react";
+
+import styles from '../styles/Login.module.css';
 
 const Login: React.FC<{}> = () => {
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log("Submitted");
+    }
+
     return (
-        <></>
+        <form className={styles.container} onSubmit={handleSubmit}>
+            <h1>Login</h1>
+
+            <div className={styles.fields}>
+                <label htmlFor="username">Username or Email: </label>
+                <input id="username" type="text" />
+
+                <label htmlFor="passwords">Password:</label>
+                <input id="password" type="text" />
+            </div>
+
+            <input type="submit" value="Login" />
+        </form>
     );
 }
 
--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { FormEvent } from "react";
+
+import styles from '../styles/Register.module.css';
 
 const Register: React.FC<{}> = () => {
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log("Submitted");
+    }
+
     return (
-        <></>
+        <form className={styles.container} onSubmit={handleSubmit}>
+            <h1>Register</h1>
+
+            <div className={styles.fields}>
+                <label htmlFor="username">Username: </label>
+                <input id="username" type="text" />
+
+                <label htmlFor="email">Email: </label>
+                <input id="email" type="text" />
+
+                <label htmlFor="passwords">Password:</label>
+                <input id="password" type="text" />
+
+                <label htmlFor="passwordRepeat">Repeat Password:</label>
+                <input id="passwordRepeat" type="text" />
+            </div>
+
+            <input type="submit" value="Register" />
+        </form>
     );
 }
 
