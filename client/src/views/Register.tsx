9ab665fa5eb9d13d20b317be9f5fe6fd071b0092
Riley Rice <riceriley59@gmail.com>
2023-08-05 21:41:54 -0700

    working on input validation for registration page

--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -1,12 +1,60 @@
-import React, { FormEvent } from "react";
+import React, { useState } from "react";
 
 import styles from '../styles/Register.module.css';
 
 const Register: React.FC<{}> = () => {
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [repeatPassword, setRepeatPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [errorMsg, setErrorMsg] = useState("")
 
-        console.log("Submitted");
+    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+
+    let validInputs = false;
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        
+        try{
+            if(validInputs) {
+                let response = await fetch("/api/create/user", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        username: name,
+                        password: password,
+                        email: email,
+                    })
+                });
+
+                resetForm();
+            }
+        }catch (err: any) {
+            console.log(err)
+        }
+    }
+
+    const validateForm = () => {
+        validInputs = true;
+        setErrorMsg("");
+
+        if(emailRegex.test(email) === false){
+            setErrorMsg(errorMsg + "That isn't a valid email address. ");
+            validInputs = false;
+        }
+
+        if (password !== repeatPassword){
+            setErrorMsg(errorMsg + "Your passwords don't match. ");
+            validInputs = false;
+        }
+    }
+
+    const resetForm = () => {
+        setName("");
+        setPassword("");
+        setEmail("");
+        setRepeatPassword("");
+        setErrorMsg("");
     }
 
     return (
@@ -15,19 +63,20 @@ const Register: React.FC<{}> = () => {
 
             <div className={styles.fields}>
                 <label htmlFor="username">Username: </label>
-                <input id="username" type="text" />
+                <input id="username" type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
 
                 <label htmlFor="email">Email: </label>
-                <input id="email" type="text" />
+                <input id="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
                 <label htmlFor="passwords">Password:</label>
-                <input id="password" type="text" />
+                <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
 
                 <label htmlFor="passwordRepeat">Repeat Password:</label>
-                <input id="passwordRepeat" type="text" />
+                <input id="passwordRepeat" type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} required />
             </div>
 
-            <input type="submit" value="Register" />
+            <input type="submit" value="Register" onClick={validateForm} />
+            <p>{ errorMsg }</p>
         </form>
     );
 }
